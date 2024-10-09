d3eb062b04b943e89a2b5bb08ee503bc3d989ddd
Paul-Nathan <pngotte@gmail.com>
2024-10-09 14:33:53 +0100

    Inserting the delete icon

--- components/TodoItem.js
+++ components/TodoItem.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import { TouchableOpacity, Text, StyleSheet } from 'react-native'
+import { View, TouchableOpacity, Text, StyleSheet } from 'react-native'
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 
 export default function TodoItem({item, handleDelete}) {
   return (
     <TouchableOpacity onPress={()=>handleDelete(item.key)}>
-        <Text style={styles.item}>
-            {item.text}
-            </Text>
+      <View style={styles.item}>
+        <MaterialIcons name = 'delete' size={18} color='#40E0D0'/>
+        <Text>{item.text}</Text>
+            </View>
     </TouchableOpacity>
   )
 }
 
 const styles = StyleSheet.create({
     item:{
+        flexDirection: 'row',
         padding:16,
         marginTop:16,
         borderColor:"#40E0D0",
